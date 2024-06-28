38ed350a65aba0ea8de9da520150f5e5abed1808
munish2580 <kumarmunish4335@gmail.com>
2024-06-28 13:40:31 +0530

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,13 @@
+a=4
+b=4
+c=8
+
+a=str(a)
+b=str(b)
+c=str(c)
+
+string=a+b+c
+final_float=float(string)
+print("final float:",final_float,type(final_float))
+final_int=int(string)
+print("final int:",final_int,type(final_int))
---
+++ whileloop.py
@@ -0,0 +1,12 @@
+x=4
+y=0
+
+while x>=0:
+ 
+ if x==y:
+  break 
+ else:
+  print(x,y)
+  x_=1
+  y+=1
+           
