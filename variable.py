765b6d026de771ff03979304501c973a9e2c1b40
munish2580 <kumarmunish4335@gmail.com>
2024-09-01 18:47:54 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,20 @@
+name ="munish"
+# print(type(name))
+rollno=21
+# print(type(rollno))
+percentage=90.3
+# print(type(percentage))
+print(name, rollno ,percentage)
+print("My name is " + name +" and my rollno is",rollno)
+print("I scored ",percentage," % in final exam ")
+# print expression
+print("my percentage has chanded to", percentage-2)
+# update data percentage
+# percentage=83
+# print(name, rollno ,percentage) 
+# print  separtor
+print(name, rollno ,percentage,sep="-")
+x=1
+y=2
+z=4
+print(x,y,z,sep="->")
